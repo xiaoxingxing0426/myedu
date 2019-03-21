1d25d9da3d6a01b367f2a78e64252319c7dde073
unknown <1026574217@qq.com>
2019-03-21 16:12:10 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,40 @@
+# model模块
+# demo例子
+# return返回
+# class 类。类型
+def str_demo():
+    aint = '1'
+    print(aint)
+    print(type(aint))
+
+
+# 同一个 层
+def add(aint, bint):
+    print(aint)
+    print(bint)
+    return aint - bint
+    # int_demo()
+    # 提取变量，选中代买的行尾，ctrl +alt +v
+
+def float_demo():
+    afloat = 1.2
+    print(afloat)
+    print(type(float))
+
+def float_demo():
+    afloat = 5.8
+    print(afloat)
+    print(type(afloat))
+
+def str_dem01():
+
+
+
+
+if __name__ == '__main__':
+    float_demo()
+
+
+
+
+    pass
